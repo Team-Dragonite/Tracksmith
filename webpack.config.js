aa63dfbaa31b49373c7bce5857d6f19d644c504e
stephenkim612 <92137370+stephenkim612@users.noreply.github.com>
2022-01-25 18:57:06 -0800

    added dashboard
    Co-authored-by: hanswand hanswand@users.noreply.github.com
    Co-authored-by: KyleBoudewyn KyleBoudewyn@users.noreply.github.com
    Co-authored-by: abeer-f abeer-f@users.noreply.github.com

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   devServer: {
     compress: true,
-    proxy: { '/api': 'http://localhost:3000' },
+    proxy: { '/': 'http://localhost:3000' },
   },
   entry: path.resolve(__dirname, './app/client/index.js'),
   output: {
