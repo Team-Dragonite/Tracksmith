ab3b669f305a6d55de8f8cafb29d24b0e5813949
Abeer Faizan <abeer-f@users.noreply.github.com>
2022-02-25 19:21:58 -0500

    Added dynamic code splitting
    Co-authored-by: hanswand hanswand@users.noreply.github.com
    Co-authored-by: Kyle Boudewyn KyleBoudewyn@users.noreply.github.com
    Co-authored-by: abeer-f abeer-f@users.noreply.github.com
    Co-authored-by: stephenkim612 stephenkim612@users.noreply.github.com

--- app/client/index.js
+++ app/client/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-import SignUp from './components/SignUpPage.jsx';
 import ConversionRates from './components/ConversionRate.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
-import Dashboard from './components/Dashboard.jsx'
+
+const App = React.lazy(() => import("./components/App.jsx"));
+const SignUp = React.lazy(() => import("./components/SignUpPage.jsx"));
+
+const Dashboard = React.lazy(() => import("./components/Dashboard.jsx"));
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
       <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/signup' element={<SignUp />} /> 
-        <Route path='/dashboard' element={<Dashboard />} /> 
-        <Route path='/conversion' element={<ConversionRates />} /> 
+        <Route path='/' element={<React.Suspense fallback={<>...</>}>
+        <App />
+      </React.Suspense>} />
+        <Route path='/signup' element={<React.Suspense fallback={<>...</>}>
+        <SignUp />
+      </React.Suspense>} /> 
+        <Route path='/dashboard' element={<React.Suspense fallback={<>...</>}>
+        <Dashboard />
+      </React.Suspense>} /> 
+        <Route path='/conversion' element={<React.Suspense fallback={<>...</>}>
+        <ConversionRates />
+      </React.Suspense>} /> 
       </Routes>
     </Provider>
   </BrowserRouter>
